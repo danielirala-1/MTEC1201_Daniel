6e42fb3b5a303ed5ed0dd22ed676efd3e28bf9c7
danielirala-1 <danielirala767@gmail.com>
2026-02-27 13:48:10 -0500

    add sketches to folder

---
+++ SS2_Daniel Irala/sketch.js
@@ -0,0 +1,23 @@
+/*skyline with a planet in the background
+*/
+function setup() {
+
+  createCanvas(400, 400);
+}let circlex = 200;
+
+function draw() {
+  background(220);
+  circle (mouseX,150,120);
+  circlex+=5
+  
+  
+  rect (200,200, 100, 200);
+  rect (50,300, 80, 100);
+  rect (300,300, 90,100);
+
+  }
+
+
+  
+  
+
