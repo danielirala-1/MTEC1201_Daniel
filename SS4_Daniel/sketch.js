b9a5a5ca7f44638a0c21f44d95fad86d2c7f597e
danielirala-1 <danielirala767@gmail.com>
2026-03-06 10:37:26 -0500

    ss4_daniel

--- SS4_Daniel/sketch.js
+++ SS4_Daniel/sketch.js
@@ -1,7 +1,38 @@
-function setup() {
-  createCanvas(400, 400);
+//SS4_DANIEL
+
+let penguin; //Declare p5.image called penguin
+
+let opacity = 0;
+let fade = 1;
+
+const millisPerSecond = 1000
+let animationStart;
+const animationTime = 3 * millisPerSecond
+
+
+
+function preload()
+{
+  
+  penguin = loadImage("images/penguin.png");
+
 }
 
-function draw() {
-  background(220);
+
+function setup()
+{
+  createCanvas(500, 500);
+  background (255);
+  imageMode (CENTER); //draws images from center point
 }
+
+function draw()
+{
+  background(255);
+  //display image with image
+  image (penguin, width / 2, height / 2);
+
+  image (penguin, width / 5, height - height / 5, penguin.width / 2, penguin.height / 2);
+}
+
+
